5ebe1f8fd8360a342043cac248caeaab7078cead
alexrodpas <alex.rodriguez.pascual@gmail.com>
2021-05-23 22:18:02 -0700

    Visualization change

--- mrtl-model/visualization/plot.py
+++ mrtl-model/visualization/plot.py
@@ -310,24 +310,24 @@ def latent_factor_polar_heatmap(X, fig_dir, b, c, draw_court, low=True, normaliz
     for k in range(shape[-1]):
         # ax.set_title('K={0}'.format(k + 1))
         if draw_court:
-            ax_court = fig.add_axes([0, 0, 1, 1])
+            ax_court = fig.add_axes([1/9, 1/7, 2/7, 5/7])
             draw_half_court_left(ax_court)
             ax_court.set_xticks([])
             ax_court.set_yticks([])
             
             data = X[..., k - 1].transpose()
 
-            ax_polar = fig.add_axes([0, 0, 1, 1], polar=True, frameon=False)
+            ax_polar = fig.add_axes([1/5, 1/5, 4/5, 4/5], polar=True, frameon=False)
             rad = np.linspace(0, 36, b[0])
             azm = np.linspace(0, np.pi, 180)
             r, th = np.meshgrid(rad, azm)
             heatmap = np.zeros((180, b[0]))
             for i in range(data.shape[0]):
                 for j in range(data[0].shape[0]):
-                    heatmap[i*180//b[1]:(i+1)*180//b[1]][j*36//b[0]:(j+1)*36//b[0]] = data[i][j]
+                    heatmap[i][j] = data[i*b[1]//180][j*b[0]//36]
     
-            ax_polar.pcolormesh(th, r, heatmap, shading='nearest', cmap=cmap, alpha=0.3)
-            ax_polar.plot(azm, r, color='k', ls='none')
+            ax_polar.pcolormesh(th, r, heatmap, shading='flat', cmap=cmap, alpha=0.3)
+            ax_polar.plot(azm, r, color='r', ls='none')
 
             plt.thetagrids(range(0, 181, 180//b[1]))
             plt.rgrids(range(0, 37, 36//b[0]))
@@ -354,7 +354,7 @@ def latent_factor_polar_heatmap(X, fig_dir, b, c, draw_court, low=True, normaliz
         else:
             data = X[..., k - 1].transpose()
 
-            ax_polar = fig.add_axes([0, 0, 1, 1], polar=True, frameon=False)
+            ax_polar = fig.add_axes([1/5, 1/5, 4/5, 4/5], polar=True, frameon=False)
             rad = np.linspace(0, 6, c[0])
             azm = np.linspace(0, 2 * np.pi, 360)
             r, th = np.meshgrid(rad, azm)
@@ -363,8 +363,8 @@ def latent_factor_polar_heatmap(X, fig_dir, b, c, draw_court, low=True, normaliz
                 for j in range(heatmap[0].shape[0]):
                     heatmap[((i - 180//c[1] - 1) % 360)][j] = data[i*c[1]//360][j*c[0]//6]
     
-            ax_polar.pcolormesh(th, r, heatmap, shading='nearest', cmap=cmap, alpha=0.3)
-            ax_polar.plot(azm, r, color='k', ls='none')
+            ax_polar.pcolormesh(th, r, heatmap, shading='flat', cmap=cmap, alpha=0.3)
+            ax_polar.plot(azm, r, color='r', ls='none')
 
             plt.thetagrids(range(180//c[1], 360 - (180//c[1]) + 1, 360//c[1]))
             plt.rgrids(range(0, 7, 6//c[0]))
