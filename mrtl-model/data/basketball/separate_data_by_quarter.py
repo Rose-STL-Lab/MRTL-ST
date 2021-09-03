c210fdbdb25d4ef23d49363ec98f37f1289dc327
alexrodpas <alex.rodriguez.pascual@gmail.com>
2021-09-03 13:23:53 -0700

    Removed redundant data

--- mrtl-model/data/basketball/separate_data_by_quarter.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-raw_data = pd.read_pickle("./full_data_bs.pkl")
-
-for i in range(4):
-    quarter_data = raw_data.loc[raw_data['quarter'] == i]
-    
-    quarter_data.to_pickle("full_data_quarter_{0}.pkl".format(i + 1))
